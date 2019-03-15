0ac2d171c1161dc68e99659f7e32bc433ee76060
AlexsJones <jonesax@hush.com>
2019-03-15 10:46:38 +0000

    removed the nifi bootstrap step

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 kubectl create ns nifi
-kubectl create -f statefulset/nifi-bootstrap.yaml --namespace=nifi
 kubectl create -f statefulset/service.yaml --namespace=nifi
 kubectl create -f statefulset/statefulset.yaml --namespace=nifi
