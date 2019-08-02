f3ff048b6324dd77bef7fbc21f5a75cfc2a69e98
Alexander Jones <alex.jones@aexp.com>
2019-08-02 09:13:26 +0100

    correct issue with deploy.sh path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 kubectl create ns nifi
-kubectl create -f statefulset/service.yaml --namespace=nifi
-kubectl create -f statefulset/statefulset.yaml --namespace=nifi
+kubectl create -f deployment/service.yaml --namespace=nifi
+kubectl create -f deployment/statefulset.yaml --namespace=nifi
